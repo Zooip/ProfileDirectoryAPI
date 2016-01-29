301f9646fa1dfddefb56a80d0b995efe8fd3ded7
Alexandre Narbonne <alexandre.narbonne@gadz.org>
2016-01-29 11:18:49 +0100

    add ressource owner finder and redirect after auth

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -2,12 +2,17 @@ class UserSessionsController < ApiEngineBaseController
   
   def new
     @user_session = UserSession.new
+    @redirect_url=redirect_params
   end
 
   def create
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
-      redirect_to home_home_path
+      if redirect_params.present?
+        redirect_to redirect_params
+      else
+        redirect_to home_home_path
+      end
     else
       render :action => :new
     end
@@ -16,6 +21,10 @@ class UserSessionsController < ApiEngineBaseController
   def destroy
     current_user_session.destroy
     redirect_to root_path
+  end
+
+  def redirect_params
+    redirect_regex = /^(?!http(s)?:\/\/).+/.match(params.fetch(:redirect,nil)).to_s
   end
 
 end
--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -4,10 +4,9 @@ Doorkeeper.configure do
 
   # This block will be called to check whether the resource owner is authenticated or not.
   resource_owner_authenticator do
-    fail "Please configure doorkeeper resource_owner_authenticator block located in #{__FILE__}"
-    # Put your resource owner authentication logic here.
+        # Put your resource owner authentication logic here.
     # Example implementation:
-    #   User.find_by_id(session[:user_id]) || redirect_to(new_user_session_url)
+    (@current_user ||= (@current_user_session||= UserSession.find) && @current_user_session.user_mockup)||  redirect_to(user_sessions_url(redirect:request.fullpath))
   end
 
   # If you want to restrict access to the web interface for adding oauth authorized applications, you need to declare the block below.
